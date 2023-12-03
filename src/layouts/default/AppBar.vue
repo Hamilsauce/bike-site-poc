49365b04debea567e1ed07f291b9e15cd1304282
jake hamilton <jacobwilsonhamilton@gmail.com>
2023-12-03 16:09:43 -0600

    finally

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -3,7 +3,7 @@
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
 
-      Essentials Preset
+      BIKE TOWN HOME OF THE BIKE SALES
     </v-app-bar-title>
   </v-app-bar>
 </template>
